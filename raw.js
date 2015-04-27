cc92b6aad964c2f18e0d217b228a918f307409e5
Jason Frame <jason@onehackoranother.com>
2015-04-27 19:01:28 +0100

    move raw functions to own file

---
+++ raw.js
@@ -0,0 +1,38 @@
+var HALF_PI = Math.PI / 2;
+
+exports.linear = function(x) {
+    return x;
+}
+
+exports.smooth = function(x) {
+    return (x*x) * (3 - (2*x));
+}
+
+exports.easeInQuad = function(x) {
+    return x * x;
+}
+
+exports.easeOutQuad = function(x) {
+    return -x * (x-2);
+}
+
+exports.easeInOutQuad = function(x) {
+    x *= 2;
+    if (x < 1) {
+        return (x*x) * 0.5;
+    } else {
+        return -0.5 * ((x-1)*(x-3)-1);
+    }
+}
+
+exports.easeInSine = function(x) {
+    return -Math.cos(x * HALF_PI) + 1;
+}
+
+exports.easeOutSine = function(x) {
+    return Math.sin(x * HALF_PI);
+}
+
+exports.easeInOutSine = function(x) {
+    return -0.5 * (Math.cos(x * Math.PI) - 1);
+}
